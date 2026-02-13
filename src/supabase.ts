0cbb26c54c27a12b2b8abf0e3bce035f36f34444
juliocrcala <jcrcala@gmail.com>
2026-02-13 19:11:21 +0000

    Updated supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://dzqwatrhficgrioidxnw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cXdhdHJoZmljZ3Jpb2lkeG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4NDE0MjMsImV4cCI6MjA4MTQxNzQyM30.4Kr3t_fHTbwjbNG-bYCmU87mlKa35LSomIy-XOruEgY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
